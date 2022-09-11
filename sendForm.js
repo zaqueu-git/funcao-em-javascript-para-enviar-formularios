5a59d73211a3b6012a7e580a17fb4bf35499f27b
Zaqueu Alves <69696525+zaqueu-git@users.noreply.github.com>
2022-09-11 16:04:31 -0300

    Update sendForm.js

--- sendForm.js
+++ sendForm.js
@@ -1,39 +1,64 @@
 document.addEventListener("DOMContentLoaded", myPage);
 
 function myPage() {
-    let formTestTag = document.querySelector('.js-form-test-tag');
-    let submitTag = formTestTag.querySelector('.js-submit-tag');
+    const formTestTag = document.querySelector('.js-form-test-tag');
+    const submitTag = formTestTag.querySelector('.js-submit-tag');
 
     formTestTag.addEventListener('submit', formTestHandler);
     submitTag.addEventListener('click', formTestHandler);
 
     function formTestHandler(event) {
         event.preventDefault();
-
-        let url = formTestTag.action;
-        let method = formTestTag.method;
-
-        let myHeaders = new Headers();
-        myHeaders.append("Cookie", document.cookie);
-
-        let formData = new FormData(formTestTag);
         
-        let requestOptions = {
-          method: method,
-          headers: myHeaders,
-          body: formData,
-        };
+        send();
+
+        function send() {
+            const url = formTestTag.action;
+            const method = formTestTag.method;
         
-        fetch(url, requestOptions)
-        .then(response => {
-            response.json().then(data => {
-                myResponseJsonHandler(data)
-            });
-        })
-        .catch(error => console.log('error', error));
+            const myHeaders = new Headers();
+            myHeaders.append("Cookie", document.cookie);
+        
+            const formData = new FormData(formTestTag);
+        
+            const requestOptions = {
+                method: method,
+                headers: myHeaders,
+                body: formData,
+            };
+        
+            fetch(url, requestOptions)
+                .then((response) => {
+                    response.json().then((data) => {
+                        responseCallback(data);
+                    });
+                })
+                .catch((error) => console.log("error", error));  
+        }
+        
+        function responseCallback(data) {
+            if(typeof data.status === "undefined" || typeof data.message === "undefined") {
+                return console.log("invalid data");
+            }
+        
+            const status = data.status;
+            const message = data.message;
+            const colors = {red: "#F2B8B8", green: "#B7E6B5", blue: "#AFDBE5", yellow: "#F2EAC7",};
+        
+            switch (status) {
+                case "success":
+                    return success();
+                default:
+                    return error();
+            }
+        
+            function success() {
+                /*-- code --*/
+            }
+        
+            function error() {
+                /*-- code --*/
+            }
+        }
     }
-
-    function myResponseJsonHandler(data) {
-        console.log(data);
-    }    
 }
